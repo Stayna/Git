842fd8364960ab04f9c39e8d6c8146ac72a45d1c
Tarregga <stayna92@gmail.com>
2019-05-05 16:23:59 +0300

    Update phpscriptforrss.php

--- phpscriptforrss.php
+++ phpscriptforrss.php
@@ -1,9 +1,6 @@
     <?php 
-
  
       $rss = "https://vkapi.ga/functional/vk2rss/rss.php?access_token=7e34d1f0146b636911112acf9b1ec307408a99a42064a17082b0387966f640168c21335aae99d9eaeb70d&id=grecha_team&count=10&include=%23memes%40grecha_team&exclude="; 
-      
-
-      $page = file_get_contents($rss); 
-      echo $page;
+            
+      echo $rss;
     ?> 
