4633dfcc5bb721dd1f910781b3f1a71116da0689
Tarregga <stayna92@gmail.com>
2019-05-05 16:17:16 +0300

    Update phpscriptforrss.php

--- phpscriptforrss.php
+++ phpscriptforrss.php
@@ -1,12 +1,12 @@
     <?php 
 
-      // далее присваиваем адресу генерируемой RSS ленты id паблика VK
+ 
       $rss = "https://vkapi.ga/functional/vk2rss/rss.php?access_token=7e34d1f0146b636911112acf9b1ec307408a99a42064a17082b0387966f640168c21335aae99d9eaeb70d&id=grecha_team&count=10&include=%23memes%40grecha_team&exclude="; 
       
-      // засасываем RSS ленту VK в переменную $page одно строкой 
+
       $page = file_get_contents($rss); 
       
-      // далее пошла жоская кастрация текста регулярками (кто в теме поймет) 
+
       $newpage = preg_replace_callback("|(CDATA\[)(.+)(\]\])|imU",  
       function ($matches) { 
         $t0 = preg_replace ("|\<\/br\>|imU", "\t", $matches[2]);     
@@ -18,7 +18,6 @@
       $t3 = preg_replace ("|http.+\.jpg|imU", "\t$0\t", $t2); 
       $text = $matches[1] . $t3 . $matches[3]; 
       $ntext = preg_replace('|[\r\n\s]+|imU', " ", $text); return $ntext; }, "$page"); 
-		
-      //Возвращаем обработанную RSS ленту 
-      echo $newpage; //конец 
+
+      echo $newpage;
     ?> 
